9086c6daa498d37f7bec03a997c8945f5d42d0a2
Eric <ericpedley@gmail.com>
2020-03-13 16:13:39 -0700

    fixed input

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,10 +1,12 @@
 package main.java;
 
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -39,12 +41,12 @@ public class Server implements Runnable {
 
 	@Override
 	public void run() {
-		BufferedInputStream in = null;
+		BufferedReader in = null;
 		BufferedOutputStream outputStream=null;
 		PrintWriter out=null;
 		try {
-			in = new BufferedInputStream(client.getInputStream());
-			System.out.println("Input line: "+in.nextLine());
+			in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			System.out.println("Input line: "+in);
 			File index = new File("public/index.html");
 			byte[] outputData = readFileData(index);
 			outputStream = new BufferedOutputStream(client.getOutputStream());
