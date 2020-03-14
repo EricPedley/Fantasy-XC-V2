b7832477d7012ad37f745b2dfb936ca5e8fa0c14
Eric <ericpedley@gmail.com>
2020-03-13 21:18:05 -0700

    added some comments and renamed some variables

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -21,12 +21,12 @@ public class Server implements Runnable {
 
 	@Override
 	public void run() {//each time this method runs it handles one request
-		BufferedReader in = null;
-		BufferedOutputStream dataOutput = null;
-		PrintWriter headerOutput = null;
+		BufferedReader in = null;//input stream
+		BufferedOutputStream dataOut = null;//output stream for body of response
+		PrintWriter headerOut = null;//output stream for head of response
 		try {
 			in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-			String rawRequest = in.readLine();
+			String rawRequest = in.readLine();//first line of request
 			if (rawRequest == null) {
 				System.out.println("null request, terminating thread");
 				return;
@@ -40,19 +40,19 @@ public class Server implements Runnable {
 					resource += "index.html";
 				}
 				File file = new File("public/" + resource);
-				String contentType = getContentType(resource);
-				byte[] outputData = readFileData(file);
-				dataOutput = new BufferedOutputStream(client.getOutputStream());
-				headerOutput = new PrintWriter(client.getOutputStream());
-				headerOutput.print("HTTP/1.1 200 OK\r\n");// you need to use \r\n instead of a println or else it crashes
-				headerOutput.print("Server: Fantasy XC Server : 1.0\r\n");
-				headerOutput.print("Date: " + new Date() + "\r\n");
-				headerOutput.print("Content-type: " + contentType + "\r\n");
-				headerOutput.print("Content-length: " + file.length() + "\r\n");
-				headerOutput.print("\r\n"); // blank line between headers and content, very important !
-				headerOutput.flush(); // flush character output stream buffer
-				dataOutput.write(outputData, 0, outputData.length);
-				dataOutput.flush();
+				String contentType = getContentType(resource);//content type is what type of file
+				byte[] outputData = readFileData(file);//converts file into array of bytes
+				dataOut = new BufferedOutputStream(client.getOutputStream());
+				headerOut = new PrintWriter(client.getOutputStream());
+				headerOut.print("HTTP/1.1 200 OK\r\n");// you need to use \r\n instead of a println or else it crashes
+				headerOut.print("Server: Fantasy XC Server : 1.0\r\n");
+				headerOut.print("Date: " + new Date() + "\r\n");
+				headerOut.print("Content-type: " + contentType + "\r\n");
+				headerOut.print("Content-length: " + file.length() + "\r\n");
+				headerOut.print("\r\n"); // blank line between headers and content, very important !
+				headerOut.flush(); // flush character output stream buffer
+				dataOut.write(outputData, 0, outputData.length);
+				dataOut.flush();
 
 			}
 
@@ -61,13 +61,12 @@ public class Server implements Runnable {
 		} finally {
 			try {
 				in.close();
-				if(headerOutput!=null) {
-					dataOutput.close();
-					headerOutput.close();
+				if(headerOut!=null) {//this condition happens when the response is null and the code to initialize the output streams never runs
+					dataOut.close();
+					headerOut.close();
 				}
 				client.close();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
