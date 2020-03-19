393d986c67d3ebc2c1e43dc9b90750acac868980
ericpedley@gmail.com <ericpedley@gmail.com>
2020-03-18 20:05:26 -0700

    added database code

---
+++ src/main/java/DatabaseConnector.java
@@ -0,0 +1,21 @@
+package main.java;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnector {
+	public void connect(String url) {
+		Connection c = null;
+	      try {
+	         Class.forName("org.postgresql.Driver");
+	         c = DriverManager
+	            .getConnection(url,
+	            "eric", "1234");
+	      } catch (Exception e) {
+	         e.printStackTrace();
+	         System.err.println(e.getClass().getName()+": "+e.getMessage());
+	         System.exit(0);
+	      }
+	      System.out.println("Opened database successfully");
+	}
+}
