c04c60543436ec777578a2ab4836566aa6a652a9
ericpedley@gmail.com <ericpedley@gmail.com>
2020-03-20 20:51:55 -0700

    idk what changed from last commit

--- public/js/index.js
+++ public/js/index.js
@@ -4,13 +4,29 @@ const e = React.createElement;
 
 function makeRequest() {
     var request = new XMLHttpRequest();
+    request.upload.timeout=5000;
     var data = {message:"hi server!!"};
+    request.upload.onprogress = function(event) {
+        console.log(`Uploaded ${event.loaded} of ${event.total} bytes`);
+      };
+      
+      request.upload.onload = function() {
+        console.log(`Upload finished successfully.`);
+      };
+      
+      request.upload.onerror = function() {
+        console.log(`Error during the upload: ${request.status}`);
+      };
+      request.timeout=500;
     request.addEventListener("load", function() {
-        console.log(request.responseText);
+        console.log(request.response);
     });
-    request.open('GET', 'http://localhost:6969/endpoint', true);
+   
+    request.open('POST', 'http://localhost:6969/endpoint', true);
+    
     request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
     request.send(JSON.stringify(data));
+    
 }
 
 class Button extends React.Component {
