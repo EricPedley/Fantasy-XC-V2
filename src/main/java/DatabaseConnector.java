a966602048233829e0166a84b83bc7a05f172b83
ericpedley@gmail.com <ericpedley@gmail.com>
2020-04-12 22:19:05 -0700

    added logging code

--- src/main/java/DatabaseConnector.java
+++ src/main/java/DatabaseConnector.java
@@ -1,25 +1,40 @@
 package main.java;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
 
-public class DatabaseConnector {
+public class DatabaseConnector {//TODO: add try/catch to both methods instead of throws
 	public void executeUpdate(String statement) throws SQLException, ClassNotFoundException {
 		Connection c = null;
-		Class.forName("org.postgresql.Driver");
-		String url = "jdbc:postgresql://ec2-52-23-14-156.compute-1.amazonaws.com:5432/dbo4aa6j9pt62c";
-		c = DriverManager.getConnection(url, "essggmgpyritgn",
-				"226d5952c81905ab5d6e176ac53e686d7fa41d25aece4f631cdebe78855a6dc8");
-		c.setAutoCommit(false);
-		Statement s = c.createStatement();
-		s.executeUpdate(statement);
-		c.commit();
-		s.close();
-		c.close();
-		System.out.println("Opened database successfully");
+		Statement s = null;
+		try {
+			Class.forName("org.postgresql.Driver");
+			String url = "jdbc:postgresql://ec2-52-23-14-156.compute-1.amazonaws.com:5432/dbo4aa6j9pt62c";
+			c = DriverManager.getConnection(url, "essggmgpyritgn",
+					"226d5952c81905ab5d6e176ac53e686d7fa41d25aece4f631cdebe78855a6dc8");
+			c.setAutoCommit(false);
+			s = c.createStatement();
+			s.executeUpdate(statement);
+			c.commit();
+		} finally {
+			s.close();
+			c.close();
+			try {
+				FileWriter f = new FileWriter("dblogs.txt");
+				f.append(statement + " | " + (new Date()) + "\n");
+				f.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			System.out.println("finished database operation(might not have been successful as this println is in the finally clause");
+		}
+
 	}
 
 	public ResultSet executeQuery(String statement) throws SQLException, ClassNotFoundException {
@@ -36,7 +51,14 @@ public class DatabaseConnector {
 		} finally {
 			s.close();
 			c.close();
-			System.out.println("Opened database successfully");
+			try {
+				FileWriter f = new FileWriter("dblogs.txt");
+				f.append(statement + " | " + (new Date()) + "\n");
+				f.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			System.out.println("finished database operation(might not have been successful as this println is in the finally clause");
 		}
 	}
 }
