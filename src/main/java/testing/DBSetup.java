5e49547c77c0a5563537b8d68072fdb4fef43dec
ericpedley@gmail.com <ericpedley@gmail.com>
2020-04-12 22:19:13 -0700

    ran some more tests

--- src/main/java/testing/DBSetup.java
+++ src/main/java/testing/DBSetup.java
@@ -1,12 +1,16 @@
 package main.java.testing;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Date;
 
 import main.java.DatabaseConnector;
 public class DBSetup {
 	public static void main(String[] args) {
 		DatabaseConnector c = new DatabaseConnector();
 		try {
-			c.executeUpdate("CREATE TABLE users (athleteID INTEGER);");
+			String query = "CREATE TABLE users (userID SERIAL PRIMARY KEY, username TEXT, password TEXT);";
+			c.executeUpdate(query);
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
