fae1255eaab5b3890a1aaf3138eaf187b21d16ec
ericpedley@gmail.com <ericpedley@gmail.com>
2020-04-15 21:51:12 -0700

    overrode forms with js

--- public/js/authforms.js
+++ public/js/authforms.js
@@ -3,7 +3,7 @@ class Login extends React.Component {
         return (
             <center style = {{backgroundColor:"FF0000"}}>
                 <h3>Login:</h3>
-                <form name = "login" method = "POST" target = "_self">
+                <form name = "login" method = "POST" target = "_self" className = "formOverride">
                     <input type = "text" name = "type" style = {{display:"none"}} value = {0}></input>
                     <input type = "text" name = "user"></input><br></br>
                     <input type = "text" name = "pass"></input><br></br>
@@ -19,7 +19,7 @@ class Signup extends React.Component {
         return (
             <center style = {{backgroundColor:"00FF00"}}>
                 <h3>Signup:</h3>
-                <form id = "signup" method = "POST" target = "_self">
+                <form id = "signup" method = "POST" target = "_self" className = "formOverride">
                 <input type = "text" name = "type" style = {{display:"none"}} value = {1}></input>
                     <input type = "text" name = "user"></input><br></br>
                     <input type = "text" name = "pass"></input><br></br>
--- public/js/index.js
+++ public/js/index.js
@@ -3,29 +3,29 @@
 const e = React.createElement;
 
 function POST(data) {
-    var request = new XMLHttpRequest();
-    request.upload.timeout=5000;
-    request.upload.onprogress = function(event) {
-        console.log(`Uploaded ${event.loaded} of ${event.total} bytes`);
-      };
-      
-      request.upload.onload = function() {
-        console.log(`Upload finished successfully.`);
-      };
-      
-      request.upload.onerror = function() {
-        console.log(`Error during the upload: ${request.status}`);
-      };
-      request.timeout=500;
-    request.addEventListener("load", function() {
-        console.log(request.response);
-    });
-   
-    request.open('POST', 'http://localhost:6969/endpoint', true);
-    
-    request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-    request.send(JSON.stringify(data));
-    
+  var request = new XMLHttpRequest();
+  request.upload.timeout = 5000;
+  request.upload.onprogress = function (event) {
+    console.log(`Uploaded ${event.loaded} of ${event.total} bytes`);
+  };
+
+  request.upload.onload = function () {
+    console.log(`Upload finished successfully.`);
+  };
+
+  request.upload.onerror = function () {
+    console.log(`Error during the upload: ${request.status}`);
+  };
+  request.timeout = 500;
+  request.addEventListener("load", function () {
+    console.log(request.response);
+  });
+
+  request.open('POST', 'http://localhost:6969/endpoint', true);
+
+  request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+  request.send(JSON.stringify(data));
+
 }
 
 function loadSignup() {
@@ -34,6 +34,33 @@ function loadSignup() {
   ReactDOM.render(e(Signup), login);
 }
 
+function overrideForms() {
+  console.log("overriding form sumbits");
+  let form = document.querySelector('.formOverride');
+  form.addEventListener("submit", function (event) {
+    let formData = new FormData(form);
+    let request = new XMLHttpRequest();
+    request.setRequestHeader
+    event.preventDefault();
+    console.log(event);
+    request.onreadystatechange = function () {
+      if (request.readyState == XMLHttpRequest.DONE) {
+        console.log(request.responseText);
+      }
+    }
+    request.open("POST", window.location.href);
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    let entries = [];
+    for (let pair of formData.entries()) {
+      entries.push(`${pair[0]}=${pair[1]}`);
+    }
+    let data = entries.join('&').replace(/%20/g, '+');
+    request.send(data);
+  });
+}
 
+window.onload = function () {
+  overrideForms();
+}
 const domContainer = document.querySelector('#login');
 ReactDOM.render(e(Login), domContainer);
