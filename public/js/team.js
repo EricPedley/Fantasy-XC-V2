beffa0b0d50cf5011ff9e2ec5940ebc3838930a4
Eric <ericpedley@gmail.com>
2020-05-01 16:01:51 -0700

    fixed missing }

--- public/js/team.js
+++ public/js/team.js
@@ -1,9 +1,9 @@
 function loadTeam() {
-    let id=-1;
-    if((id=localStorage.getItem('id'))) {
+    let id = -1;
+    if ((id = localStorage.getItem('id'))) {
         getRoster(id);
     }
-    console.log("load team event, id is "+id);
+    console.log("load team event, id is " + id);
     ReactDOM.unmountComponentAtNode(content);
     ReactDOM.render(e(TeamView), content);
 }
@@ -12,7 +12,7 @@ class TeamView extends React.Component {
         return (
             <div>
                 <center><h2>Next Meet</h2></center>
-                <div className="row" style={{height:"50vh"}}>
+                <div className="row" style={{ height: "50vh" }}>
                     <div className="col-md-6" style={{ backgroundColor: "#00FF00" }}>Roster</div>
                     <div className="col-md-6" style={{ backgroundColor: "#FF0000" }}>Bench</div>
                 </div>
@@ -26,16 +26,19 @@ class TeamView extends React.Component {
 function getRoster(id) {
     console.log("getting roster");
     let request = new XMLHttpRequest();
-    let target = window.location.href.substring(0,window.location.href.indexOf("#"))+`Rosters?id=${id}&meetID=${1}`;//TODO make meet ID dynamic
-    request.open("GET",target);
-    request.onreadystatechange = function() {
-        if(request.readyState==XMLHttpRequest.DONE) {
+    let target = window.location.href.substring(0, window.location.href.indexOf("#")) + `Rosters?id=${id}&meetID=${1}`;//TODO make meet ID dynamic
+    request.open("GET", target);
+    request.onreadystatechange = function () {
+        if (request.readyState == XMLHttpRequest.DONE) {
             console.log(request.responseText);
             return request.responseText;//this return statement never runs b/c of async stuff, need to use a promise or something
+        }
     }
-    request.setRequestHeader("Content-Type","text/plain");
+    request.setRequestHeader("Content-Type", "text/plain");
     request.send("it won't work without body text for some reason");
 }
 
+
 function testGET() {
+    console.log("debug button for later use");
 }
