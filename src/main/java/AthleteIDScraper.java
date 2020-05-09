b4ee1c11699ac02781d98558c07ef207aef7ff0b
Eric <ericpedley@gmail.com>
2020-05-09 15:29:36 -0700

    added html parsing code

--- src/main/java/AthleteIDScraper.java
+++ src/main/java/AthleteIDScraper.java
@@ -14,7 +14,7 @@ public class AthleteIDScraper {
 
 	public static void scrapeIDs() {
 
-		String host = "https://www.athletic.net/CrossCountry/";
+		String host = "https://www.athletic.net/CrossCountry/School.aspx?SchoolID=1096";
 		URL url;
 		try {
 			url = new URL(host);
@@ -26,11 +26,21 @@ public class AthleteIDScraper {
 			String inputLine;
 			StringBuffer content = new StringBuffer();
 			while ((inputLine = in.readLine()) != null) {
-				content.append(inputLine);
+				content.append(inputLine+"\n");
 			}
-			System.out.println(content);
 			in.close();
 			con.disconnect();
+			String html = content.toString();
+			int sI = html.indexOf("\"athletes\":");// short for "start index"
+			int eI = html.indexOf("\"coaches\":");// short for "end index"
+			String jsonString = "{\n";
+			for (int i = html.indexOf("\"ID\"", sI); i != -1 && i < eI; i = html.indexOf("\"ID\":", i + 1)) {
+				String id = html.substring(i + 5, i + 13);
+				String name = html.substring(i + 22, html.indexOf(" ", i + 22));
+				jsonString += "\"" + id + "\":" + "\"" + name + "\",\n";
+			}
+			jsonString = jsonString.substring(0, jsonString.length() - 2) + "\n}";
+			System.out.println(jsonString);
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
