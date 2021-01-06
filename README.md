0f5398c09839c6f312cac8c7a5d758b997bac51b
Eric Pedley <ericpedley@gmail.com>
2021-01-06 14:04:11 -0800

    set up gh pages

--- README.md
+++
@@ -1,1 +0,0 @@
-# Fantasy-XC-V2
--- src/main/java/endpoints/Endpoint.java
+++
@@ -1,9 +0,0 @@
-package main.java.endpoints;
-
-import org.json.JSONObject;
-
-public interface Endpoint {
-	public abstract String handleGET(JSONObject params);
-	public abstract String handlePOST(JSONObject data);
-	public abstract void handlePATCH(JSONObject data);
-}
